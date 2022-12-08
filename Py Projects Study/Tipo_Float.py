f8e33952f272d780742de12635b6a06a79f8a80c
Matheus Do Amaral Genta Mansano <69807037+devMansano@users.noreply.github.com>
2022-12-08 13:59:57 -0300

    Add files via upload

---
+++ Py Projects Study/Tipo_Float.py
@@ -0,0 +1,32 @@
+"""
+Tipo Float (tipo real, decimal)
+Separador de casas na programação é o ponto e não a vírgula
+
+Gera uma tupla ao invés de um float
+valor = 1,52
+
+
+
+Dupla atribuição de valor
+valor1, valor2 = 1, 42
+
+
+
+Converter um float para um int
+res = int(valor)
+print(res)
+OBS : ao converter valor pra inteiro perdemos precisão
+
+
+Variável complex
+variavel = 5j
+variavel = variavel ** 2
+
+
+"""
+
+
+
+valor = 1.254
+print(valor)
+print(type(valor))
